3d8b9824c53709a4787def2a354e97942dd90958
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-19 17:24:25 -0700

    updated file

--- functions.php
+++ functions.php
@@ -6,18 +6,17 @@
  *
  * @package   Initiator
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (C) 2019-2023. Benjamin Lu
+ * @copyright 2019-2023. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://github.com/benlumia007/initiator
  */
 
-# ------------------------------------------------------------------------------
-# Load the composer files
-# ------------------------------------------------------------------------------
-#
-# Load the composer files. Note that autoloading should happen first so that
-# any classes or functions are available that we might need.
+/** ------------------------------------------------------------------------------------------
+ * Bootstrap the theme.
+ * -------------------------------------------------------------------------------------------
+ * Load the bootstrap files. Note that autoload should happen first so that any classes or
+ * functions are available that we might need.
+ */
 
-if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
-	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
-}
+require_once get_parent_theme_file_path( 'app/bootstrap-autoload.php' );
+require_once get_parent_theme_file_path( 'app/framework.php' );
